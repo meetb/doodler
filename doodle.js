7498a755dea711e21412b6e6160412ec4ef5f96e
pbardea <pbardea@gmail.com>
2014-11-01 19:31:55 -0400

    Removed more useless js

--- doodle.js
+++ doodle.js
@@ -223,13 +223,13 @@ function thicknessSelectListener (e) {
   //   attrScope (The room)
   //   attrOptions (An integer whose bits specify options. "4" means
   //                the attribute should be shared).
-  msgManager.sendUPC(UPC.SET_CLIENT_ATTR,
-                     orbiter.getClientID(),
-                     "",
-                     Attributes.THICKNESS,
-                     newThickness,
-                     roomID,
-                     "4");
+  // msgManager.sendUPC(UPC.SET_CLIENT_ATTR,
+  //                    orbiter.getClientID(),
+  //                    "",
+  //                    Attributes.THICKNESS,
+  //                    newThickness,
+  //                    roomID,
+  //                    "4");
   // After the user selects a value in the drop-down menu, the iPhone
   // automatically scrolls the page, so scroll back to the top-left.
   iPhoneToTop();

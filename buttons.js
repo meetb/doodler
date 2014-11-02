25c126eb93a2f0ce5a670f8d2691778d9927ae02
meetb <meetbhvsr@gmail.com>
2014-11-02 01:12:59 -0500

    Added index file and corresponding css and js files

---
+++ buttons.js
@@ -0,0 +1,13 @@
+/*buttonOne = document.getElementById("button1");
+buttonTwo = document.getElementById("button2");
+buttonThree = document.getElementById("button3");*/
+
+document.getElementById('button1').onclick = function() {
+    alert("button was clicked");
+}​;
+document.getElementById('button2').onclick = function() {
+    alert("button was clicked");
+}​;
+document.getElementById('button3').onclick = function() {
+    alert("button was clicked");
+}​;
